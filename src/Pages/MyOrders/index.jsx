51baf8eeea8974f7efcdc9fdf27edb1a5c653de5
LAPTOP-VUQ1J6NP\Victor_Moreno <victorsahir1011@gmail.com>
2023-11-06 17:47:39 -0500

    Se finaliza la presentación de mis ordenes de compra y se enlaza con el historial de ordenes.

--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,11 +1,30 @@
+import { useContext } from "react";
 import Layout from '../../Components/Layout'
+import { Link } from "react-router-dom"
+import OrdersCard from '../../Components/OrdersCard'
+import { ShoppingCartContext } from "../../Context";
 
 function MyOrders() {
-    return (
-      <Layout>
-        MyOrders
-      </Layout>
-    );
-  }
-  
-  export default MyOrders;
+  const context = useContext(ShoppingCartContext);
+
+  return (
+    <Layout>
+      <div className="flex items-center justify-center relative w-80 mb-4">
+        <h1 className="font-medium text-xl">MyOrders</h1>
+      </div>
+      {context.order.map((order, index) => (
+        <>
+          <Link to={`/my-orders/${index}`}>
+            <OrdersCard
+              key={index}
+              totalPrice={order.totalPrice}
+              totalProducts={order.totalProducts}
+            />
+          </Link>
+        </>
+      ))}
+    </Layout>
+  );
+}
+
+export default MyOrders;
